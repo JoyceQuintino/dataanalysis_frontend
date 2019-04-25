15f5bbe1286e6326cbe2efe32eff3eceb0224c5b
Joyce Quintino <joycequintino11@gmail.com>
2019-04-25 14:11:16 -0300

    Atualização do layout

--- testeTableau/tableaupublic/js/index.js
+++ testeTableau/tableaupublic/js/index.js
@@ -1,6 +1,45 @@
-function initViz() {
+function initVizIndex() {
     var containerDiv = document.getElementById("vizContainer"),
         url = "https://public.tableau.com/shared/X22KGSTDB?:display_count=yes",
+        options = {
+            hideTabs: true,
+            onFirstInteractive: function () {
+                console.log("Run this code when the viz has finished loading.");
+            }
+        };
+
+    var viz = new tableau.Viz(containerDiv, url, options);
+}
+
+function initVizFavorecidos() {
+    var containerDiv = document.getElementById("vizContainer"),
+        url = "https://public.tableau.com/shared/C7CF9B3T2?:display_count=yes",
+        options = {
+            hideTabs: true,
+            onFirstInteractive: function () {
+                console.log("Run this code when the viz has finished loading.");
+            }
+        };
+
+    var viz = new tableau.Viz(containerDiv, url, options);
+}
+
+function initVizInstituicao() {
+    var containerDiv = document.getElementById("vizContainer"),
+        url = "https://public.tableau.com/shared/MRGYT6CJR?:display_count=yes",
+        options = {
+            hideTabs: true,
+            onFirstInteractive: function () {
+                console.log("Run this code when the viz has finished loading.");
+            }
+        };
+
+    var viz = new tableau.Viz(containerDiv, url, options);
+}
+
+function initVizProjeto() {
+    var containerDiv = document.getElementById("vizContainer"),
+        url = "https://public.tableau.com/shared/C2MJY9C98?:display_count=yes",
         options = {
             hideTabs: true,
             onFirstInteractive: function () {
